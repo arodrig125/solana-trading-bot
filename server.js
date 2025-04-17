004400dea7433cde939b700243e406f5049064e5
arodrig125 <165452926+arodrig125@users.noreply.github.com>
2025-04-16 22:44:12 -0700

    Fix build process for Digital Ocean App Platform

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
